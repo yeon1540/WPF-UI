f49fd8157d4cba8db5c6937aee3a29a4f10a39f5
yeontaegu <yon1540@naver.com>
2024-05-23 00:46:17 +0900

    1차 최종

---
+++ WPF.UI.Default/src/WPF.UI.ControlLib/ViewsModel/Pages/AutoScreenViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF.UI.ControlLib.ViewsModel.Pages
+{
+    public partial class AutoScreenViewModel
+    {
+    }
+}
