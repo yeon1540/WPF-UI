c6c0cbf71b558b5f864576f570cf5434c21aae6b
yeon <yon1540@naver.com>
2024-05-29 00:18:31 +0900

    기존 프로젝트 지우고 새로 구성.

---
+++ Defalut/Defalut/Services/ApplicationHostService.cs
@@ -0,0 +1,61 @@
+﻿using Defalut.Views;
+using DefalutControls.Views;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using Wpf.Ui;
+
+namespace Defalut.Services
+{
+    public class ApplicationHostService : IHostedService
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        private INavigationWindow _navigationWindow;
+
+        public ApplicationHostService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        /// <summary>
+        /// Triggered when the application host is ready to start the service.
+        /// </summary>
+        /// <param name="cancellationToken">Indicates that the start process has been aborted.</param>
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await HandleActivationAsync();
+        }
+
+        /// <summary>
+        /// Triggered when the application host is performing a graceful shutdown.
+        /// </summary>
+        /// <param name="cancellationToken">Indicates that the shutdown process should no longer be graceful.</param>
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Creates main window during activation.
+        /// </summary>
+        private async Task HandleActivationAsync()
+        {
+            if (!Application.Current.Windows.OfType<MainWindow>().Any())
+            {
+                _navigationWindow = _serviceProvider.GetService(typeof(INavigationWindow)) as INavigationWindow;
+                if (_navigationWindow != null)
+                {
+                    _navigationWindow.ShowWindow();
+
+                    _navigationWindow.Navigate(typeof(AutoScreen));
+                }
+            }
+
+            await Task.CompletedTask;
+        }
+    }
+}
