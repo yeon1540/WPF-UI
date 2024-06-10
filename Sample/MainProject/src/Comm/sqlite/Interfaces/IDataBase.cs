4776c242253ac03ea6aae4a790f6e639c267be47
yeon <yon1540@naver.com>
2024-06-11 00:18:05 +0900

    다시 가져옴.

---
+++ Sample/MainProject/src/Comm/sqlite/Interfaces/IDataBase.cs
@@ -0,0 +1,73 @@
+﻿using Comm.sqlite.Models;
+using System.Collections.Generic;
+
+namespace Comm.sqlite.Interfaces
+{
+    public interface IDataBase<T>
+    {
+        #region 조회
+
+        /// <summary>
+        /// 모든 데이터 조회
+        /// </summary>
+        /// <returns></returns>
+        List<T> Get();
+
+        /// <summary>
+        /// ID로 특정 데이터 조회
+        /// </summary>
+        /// <param name="sID"></param>
+        /// <returns></returns>
+        T GetDetail(string sID);
+
+        /// <summary>
+        /// ID기준으로 오름차순으로 리스트 가져오기
+        /// </summary>
+        /// <returns></returns>
+        List<T> AscOrderByGet();
+
+        /// <summary>
+        /// ID기준으로 내림차순으로 리스트 가져오기
+        /// </summary>
+        /// <returns></returns>
+        List<T> DesOrderByGet();
+
+        #endregion
+
+        #region 추가
+
+        /// <summary>
+        /// 데이터 추가
+        /// </summary>
+        /// <param name="entity"></param>
+        void Insert(string sID, string sData);
+
+        #endregion
+
+        #region 수정
+
+        /// <summary>
+        /// 데이터 업데이트
+        /// </summary>
+        /// <param name="entity"></param>
+        void Update(string sID, string sData);
+
+        /// <summary>
+        /// 데이터 업데이트
+        /// </summary>
+        /// <param name="info"></param>
+        void Update(Info info);
+
+        #endregion
+
+        #region 삭제
+
+        /// <summary>
+        /// 특정 데이터 삭제
+        /// </summary>
+        /// <param name="sID"></param>
+        void Delete(string sID);
+
+        #endregion
+    }
+}
---
+++ Sample/MainProject/src/Comm/sqlite/Models/DefaultContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Comm.sqlite.Models
+{
+    public partial class DefaultContext : DbContext
+    {
+        public DefaultContext()
+        {
+        }
+
+        public DefaultContext(DbContextOptions<DefaultContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Info> Info { get; set; }
+        public virtual DbSet<Serial> Serial { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite("Data Source=D:\\DB\\Default.db");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Info>(entity =>
+            {
+                entity.ToTable("info");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Comment).IsRequired();
+            });
+
+            modelBuilder.Entity<Serial>(entity =>
+            {
+                entity.HasKey(e => e.Number);
+
+                entity.Property(e => e.Number).ValueGeneratedNever();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
